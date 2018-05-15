10dcd88d1a015dd62715e7f93e641e34f06d747f
Harish Murali <harish02murali@gmail.com>
2018-05-16 02:33:38 +0530

    98% accuracy in 1000 epochs

--- nn.py
+++ nn.py
@@ -4,21 +4,47 @@ import numpy as np
 from sklearn.utils import shuffle
 
 
-input_file = "ToHarish/params.csv"
+testdata = "test_data.csv"
+traindata = "train_data.csv"
 
-cols = pd.read_csv(input_file, nrows=1).columns
-n_params = len(cols) - 2
+df_train = pd.read_csv(traindata)
+df_test = pd.read_csv(testdata)
+cols = pd.read_csv(traindata, nrows=1).columns
+n_params = len(cols)-1
 
-df = pd.read_csv(input_file, usecols=cols[1:], index_col=False)
-df = shuffle(df).reset_index(drop=True)
-
-df_train = df.loc[:int(len(df)/2)-1].reset_index(drop=True)
-df_test = df.loc[int(len(df)/2):].reset_index(drop=True)
-
-x_test = df_test[cols[1:-1]].as_matrix()
-y_test = df_test[cols[-1]].as_matrix()
-x_train = df_train[cols[1:-1]].as_matrix()
-y_train = df_train[cols[-1]].as_matrix()
+x_test = df_test[cols[:-1]].as_matrix()
+y_test = df_test[cols[-1]].as_matrix().astype(int)
+x_train = df_train[cols[:-1]].as_matrix()
+y_train = df_train[cols[-1]].as_matrix().astype(int)
 
 x = tf.placeholder(dtype = tf.float32, shape = [None, n_params])
-y = tf.placeholder(dtype = tf.int32, shape = [None])
+y = tf.placeholder(dtype = tf.int32, shape = [None, 1])
+
+learning_rate = 0.3
+epochs = 10
+batch_size = 100
+hidden_layer1 = 15
+
+hl1 = tf.layers.dense(x, hidden_layer1, kernel_initializer=tf.zeros_initializer(), activation=tf.nn.sigmoid)
+y_ = tf.layers.dense(hl1, 1, kernel_initializer=tf.zeros_initializer(), activation=tf.nn.sigmoid)
+loss = tf.losses.sigmoid_cross_entropy(y, y_)
+optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
+
+output = tf.cast(y_ + 0.5, tf.int32)
+correct_prediction = tf.add(tf.multiply(output, y), tf.multiply(1-output, 1-y))
+accuracy = tf.reduce_mean(tf.to_float(correct_prediction))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    acc = sess.run(accuracy, feed_dict={x:x_test, y:y_test.reshape(-1,1)})
+    print("Initial prediction error on test data: ", 100*acc, "%")
+    c = 1
+    for i in range(100000):
+        _, c = sess.run([optimiser, loss], 
+                     feed_dict={x: x_train, y: y_train.reshape(-1,1)})
+        if (i%1000 == 0):
+            acc = sess.run(accuracy,
+                           feed_dict={x: x_train, y: y_train.reshape(-1,1)})
+            print("Loss = ", c, ", Prediction error = ", 100*acc, "%")
+    acc = sess.run(accuracy, feed_dict={x:x_test, y:y_test.reshape(-1,1)})
+    print("Prediction Error on test data: ", 100*acc, "%")
--- readfiles.py
+++ readfiles.py
@@ -1,9 +1,13 @@
 import glob
+import pandas as pd
+from sklearn.utils import shuffle
 
 noise_files = glob.glob("C1/*")
 signal_files = glob.glob("IFAR8/*")
 params = ["netcc", "rho", "netED", "norm"]
-columns = "filename,netED,norm,rho0,rho1,netcc0,netcc1,label"
+columns = "netED,norm,rho0,rho1,netcc0,netcc1,label"
+
+n_params = len(columns.split(","))
 
 with open("params.csv", "w") as f_out:
     f_out.write(columns + "\n")
@@ -20,7 +24,7 @@ with open("params.csv", "w") as f_out:
                     if name == "netcc":
                         tmp = d[i].split()[1:3]
                     line += ",".join(tmp) + ","
-            f_out.write(file + line[:-1] + ",1\n")
+            f_out.write(line[1:-1] + ",1\n")
     for file in noise_files:
         with open(file, "r") as f:
             d = f.readlines()
@@ -34,20 +38,16 @@ with open("params.csv", "w") as f_out:
                     if name == "netcc":
                         tmp = d[i].split()[1:3]
                     line += ",".join(tmp) + ","
-            f_out.write(file + line[:-1] + ",0\n")
+            f_out.write(line[1:-1] + ",0\n")
 
 input_file = "params.csv"
 
 cols = pd.read_csv(input_file, nrows=1).columns
-n_params = len(cols) - 2
-
-df = pd.read_csv(input_file, usecols=cols[1:], index_col=False)
+df = pd.read_csv(input_file, index_col=False)
 df = shuffle(df).reset_index(drop=True)
 
-df_train = df.loc[:int(len(df)/2)-1].reset_index(drop=True)
-df_test = df.loc[int(len(df)/2):].reset_index(drop=True)
+df_train = df.iloc[:int(len(df)/2)].reset_index(drop=True)
+df_test = df.iloc[int(len(df)/2):].reset_index(drop=True)
 
-x_test = df_test[cols[1:-1]].as_matrix()
-y_test = df_test[cols[-1]].as_matrix()
-x_train = df_train[cols[1:-1]].as_matrix()
-y_train = df_train[cols[-1]].as_matrix()
+df_test.to_csv("test_data.csv", index=False)
+df_train.to_csv("train_data.csv", index=False)
