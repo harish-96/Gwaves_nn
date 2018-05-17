b7800732c8293c3321b1bd7580e83811fab4f8b7
Harish Murali <harish02murali@gmail.com>
2018-05-17 15:40:10 +0530

    snrs

--- nn.py
+++ nn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 
 
 testdata = "5params_test_data.csv"
@@ -35,32 +36,40 @@ correct_prediction = tf.add(tf.multiply(output, y), tf.multiply(1-output, 1-y))
 accuracy = tf.reduce_mean(tf.to_float(correct_prediction))
 
 saver = tf.train.Saver()
-# sess = tf.Session()
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    acc = sess.run(accuracy, feed_dict={x:x_test, y:y_test.reshape(-1,1)})
-    print("Initial prediction error on test data: ", 100*acc, "%")
-    c = 1
-    for i in range(epochs):
-        _, c = sess.run([optimiser, loss], 
-                     feed_dict={x: x_train, y: y_train.reshape(-1,1)})
-        if (i%1000 == 0):
-            acc = sess.run(accuracy,
-                           feed_dict={x: x_train, y: y_train.reshape(-1,1)})
-            print("Loss = ", c, ", Prediction error = ", 100*acc, "%")
-    # import pdb; pdb.set_trace()
-    acc = sess.run(accuracy, feed_dict={x:x_test, y:y_test.reshape(-1,1)})
-    pred = sess.run(output, feed_dict={x:x_test, y:y_test.reshape(-1,1)}).reshape(-1)
+sess = tf.Session()
 
-    false_positives = df_test.as_matrix()[np.where((pred - y_test) == 1)[0]][:, 0]
-    false_negatives = df_test.as_matrix()[np.where((pred - y_test) == -1)[0]][:, 0]
-    fp = df_test.loc[df_test['filename'].isin(false_positives)]
-    fn = df_test.loc[df_test['filename'].isin(false_negatives)]
+sess.run(tf.global_variables_initializer())
+acc = sess.run(accuracy, feed_dict={x:x_test, y:y_test.reshape(-1,1)})
+print("Initial prediction error on test data: ", 100*acc, "%")
+c = 1
+for i in range(epochs):
+    _, c = sess.run([optimiser, loss], 
+                 feed_dict={x: x_train, y: y_train.reshape(-1,1)})
+    if (i%1000 == 0):
+        acc = sess.run(accuracy,
+                       feed_dict={x: x_train, y: y_train.reshape(-1,1)})
+        print("Loss = ", c, ", Prediction error = ", 100*acc, "%")
+# import pdb; pdb.set_trace()
+acc = sess.run(accuracy, feed_dict={x:x_test, y:y_test.reshape(-1,1)})
+pred = sess.run(output, feed_dict={x:x_test, y:y_test.reshape(-1,1)}).reshape(-1)
 
-    signal = df_test.loc[df_test['label'] == 1]
-    noise = df_test.loc[df_test['label'] == 0]
+false_positives = df_test.as_matrix()[np.where((pred - y_test) == 1)[0]][:, 0]
+false_negatives = df_test.as_matrix()[np.where((pred - y_test) == -1)[0]][:, 0]
+fp = df_test.loc[df_test['filename'].isin(false_positives)].reset_index(drop=True)
+fn = df_test.loc[df_test['filename'].isin(false_negatives)].reset_index(drop=True)
 
-    print("Prediction Error on test data: ", 100*acc, "%")
+signal = df_test.loc[df_test['label'] == 1].reset_index(drop=True)
+noise = df_test.loc[df_test['label'] == 0].reset_index(drop=True)
 
-    save_path = saver.save(sess, "checkpoints/model.ckpt")
-    print("Model saved in path: %s" % save_path)
+print("Prediction Error on test data: ", 100*acc, "%")
+
+save_path = saver.save(sess, "checkpoints/model.ckpt")
+print("Model saved in path: %s" % save_path)
+
+sess.close()
+
+plt.semilogy(fp['snr0'], label="False Postives")
+plt.semilogy(fn['snr0'], label="False Negatives")
+plt.ylabel("SNR")
+plt.legend()
+plt.show()
